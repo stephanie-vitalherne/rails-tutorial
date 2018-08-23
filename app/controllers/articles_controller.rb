3b1d0414ab32e6367925820cc4fec4ce2b6f7539
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-23 17:48:13 -0400

    added new routes to article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,9 +3,18 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def create
+    @article = Article.new(article_params)
+  end
+
   def edit; end
 
   def show; end
 
   def index; end
+end
+private
+  def article_params
+    params.require(:article).permit(:title, :author, :content)
+  end
 end
