7ec110cb04d9c06afa40dfeb42a2ebe38a2e8956
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-23 17:35:00 -0400

    created articles table and controller for article. added a line to display it on the new page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'pages#home'
+
+  get 'articles/new'
+  get 'articles/edit'
+  get 'articles/show'
+  get 'articles/index'
   get 'pages/home'
   get 'pages/about'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
