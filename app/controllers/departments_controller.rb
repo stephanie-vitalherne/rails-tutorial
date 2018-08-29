6d798951ad1285faf62eaef8aada3e80d7b540c6
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-29 15:08:24 -0400

    added a controller new so the company id can attach itself to the department that it owns

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -1,13 +1,11 @@
 class DepartmentsController < ApplicationController
   def new
+    @company = Company.find(params[:company_id])
   end
 
-  def edit
-  end
+  def edit; end
 
-  def show
-  end
+  def show; end
 
-  def index
-  end
+  def index; end
 end
