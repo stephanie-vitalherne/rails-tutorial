a0b9faecaf5e3ab17ee8c85e070756be9ad83e91
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-29 11:05:46 -0400

    add faker to generate articles to work with

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+500.times do
+  r = (3..10).to_a.sample
+  art = Article.new(
+    title: Faker::Book.title,
+    content: Faker::Lorem.paragraphs(r),
+    user_id: [1, 2].sample
+  )
+
+  match = Article.find_by(title: art.title)
+  art.save unless match
+end
