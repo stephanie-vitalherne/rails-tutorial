56989aa24762ff23d24f60b837fedcbbc7162bbb
Stephanie Vital-Herne <vitalherne@gmail.com>
2018-08-29 15:11:16 -0400

    created the dept save

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -4,6 +4,17 @@ class DepartmentsController < ApplicationController
     @dept = Department.new
   end
 
+  def create
+    @company = Company.find(params[:company_id])
+    @dept = Department.new(dept_params)
+    if @dept.save
+      msgg = "New Department created! Let's get you some employees"
+      redirect_to @dept
+    else
+      render 'new'
+    end
+  end
+
   def edit; end
 
   def show; end
